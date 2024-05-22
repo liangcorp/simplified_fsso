02d12f75fce80d68945efaccb90b153d7662e589
Chen Liang <chen.liang.cn@outlook.com>
2024-05-22 21:09:06 +0800

    update code to python 3

--- federated_single_sign_on/sp/python/client/sp_ssh_client_sim.py
+++ federated_single_sign_on/sp/python/client/sp_ssh_client_sim.py
@@ -12,27 +12,30 @@ import getpass
 from Ticket import TicketGrantingTicket
 from Ticket import ServiceTicket
 
-HOST = socket.gethostname() # The host name
-IP = socket.gethostbyname(HOST) # The IP address
-KRB_HOST = "127.0.0.1" # Kerberos key Distribution Centre Hostname
-KRB_PORT = 50001 # The KDC simulation's port number
-SSHD_HOST = "127.0.0.1" # SSH Server Hostname
-SSHD_PORT = 2222 # The SSH server simulation's port number
+HOST = socket.gethostname()  # The host name
+IP = socket.gethostbyname(HOST)  # The IP address
+KRB_HOST = "127.0.0.1"  # Kerberos key Distribution Centre Hostname
+KRB_PORT = 50001  # The KDC simulation's port number
+SSHD_HOST = "127.0.0.1"  # SSH Server Hostname
+SSHD_PORT = 2222  # The SSH server simulation's port number
 
 # Make sure the end-user has registered in KDC
+
+
 def authentication():
     try:
-        krb_sok = socket.socket (socket.AF_INET, socket.SOCK_STREAM) # KRB socket
+        krb_sok = socket.socket(
+            socket.AF_INET, socket.SOCK_STREAM)  # KRB socket
         krb_sok.connect((KRB_HOST, KRB_PORT))
-        print 'Connecting to', HOST, '...'
-        
+        print('Connecting to', HOST, '...')
+
         name = raw_input('Username: ')
-        password = getpass.getpass('Password: ') 
-        
+        password = getpass.getpass('Password: ')
+
         krb_sok.send('req_tgt;' + name + ';' + password)
         # Recieve returned ticket information
         ticket = krb_sok.recv(1024)
-        
+
         if ticket != 'fail':
             # Convert into ticket
             save_tgt(ticket)
@@ -42,42 +45,47 @@ def authentication():
             krb_sok.close()
             return False
     except (KeyboardInterrupt, SystemExit):
-        print "existing..."
+        print("existing...")
         krb_sok.close
     else:
         pass
 
 # Save ticket granting ticket from KDC
+
+
 def save_tgt(tgt_info):
     # Set ticket granting ticket content in memory
     tgt.set_ticket_info(tgt_info)
-    print 'Recieved and saved Ticket Granting Ticket.'
+    print('Recieved and saved Ticket Granting Ticket.')
 
     # Save ticket granting ticket content in file
     f = open('tgt.key', 'w')
     f.write(tgt_info)
     f.close()
 
+
 def get_tgt_info():
-    print '\nTicket Granting Ticket:'
-    print 'Username:', tgt.get_username()
-    print 'Password:', tgt.get_password()
-    print 'Role:', tgt.get_role()
-    print 'Domain:', tgt.get_domain()
-    print 'Ticket Create Date:', tgt.get_create_date()
-    print 'Ticket Expire Date:', tgt.get_expire_date()
-    print 'Key:', tgt.get_md5sum()
-    print '\n'
+    print('\nTicket Granting Ticket:')
+    print('Username:', tgt.get_username())
+    print('Password:', tgt.get_password())
+    print('Role:', tgt.get_role())
+    print('Domain:', tgt.get_domain())
+    print('Ticket Create Date:', tgt.get_create_date())
+    print('Ticket Expire Date:', tgt.get_expire_date())
+    print('Key:', tgt.get_md5sum())
+    print('\n')
 
 # Get service ticket from KDC to access services
+
+
 def request_service_ticket():
-    krb_sok = socket.socket (socket.AF_INET, socket.SOCK_STREAM) # KRB socket
+    krb_sok = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # KRB socket
     f = open('tgt.key', 'r')
     tgt_info = f.readline()
     f.close()
 
     krb_sok.connect((KRB_HOST, KRB_PORT))
-    print 'Connecting to', HOST, '...'
+    print('Connecting to', HOST, '...')
     krb_sok.send('req_st;' + tgt_info)
 
     answer = krb_sok.recv(1024)
@@ -92,14 +100,18 @@ def request_service_ticket():
         return st_info
 
 # Simple simulation of ls
+
+
 def ls_sim():
     for dirname, dirnames, filenames in os.walk('.'):
         for subdirname in dirnames:
-            print os.path.join(dirname, subdirname)
+            print(os.path.join(dirname, subdirname))
         for filename in filenames:
-            print os.path.join(dirname, filename)
+            print(os.path.join(dirname, filename))
 
 # Simluation of a local Unix/Linux shell
+
+
 def shell_sim():
     while True:
         command = raw_input('local shell~>')
@@ -108,58 +120,60 @@ def shell_sim():
         elif command == 'exit':
             sys.exit(0)
         elif command == 'hostname':
-            print HOST
+            print(HOST)
         elif command == 'ifconfig':
-            print IP
+            print(IP)
         elif command == 'ssh foreign.virtual.vm':
-            st_info = request_service_ticket() # Request Service Ticket
+            st_info = request_service_ticket()  # Request Service Ticket
             if st_info != 'fail':
-                ssh_sim(st_info) # Simulation of SSH
+                ssh_sim(st_info)  # Simulation of SSH
             else:
-                print 'Fail to request service ticket'
+                print('Fail to request service ticket')
         elif command == 'klist':
             get_tgt_info()
         else:
-            print 'ls\tifconfig\thostname\texit\tklist\tssh foreign.virtual.vm'
+            print('ls\tifconfig\thostname\texit\tklist\tssh foreign.virtual.vm')
+
 
 def ssh_sim(st_info):
-    ssh = socket.socket (socket.AF_INET, socket.SOCK_STREAM) # SSH socket
+    ssh = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # SSH socket
     ssh.connect((SSHD_HOST, SSHD_PORT))
     # Send service ticket to ssh server
     ssh.send(st_info)
-    
+
     # Recieve single sign-on reply
     shell_name = ssh.recv(1024)
 
     if shell_name != 'fail':
         results = ''
-        
+
         while results != 'exit_confirm':
             command = raw_input(shell_name)
             ssh.send(command)
             results = ssh.recv(1024)
             if results != 'exit_confirm':
-                print results
+                print(results)
             else:
                 pass
 
     else:
-        print 'Service ticket failed to authenticate'
+        print('Service ticket failed to authenticate')
 
     ssh.close()
-    print 'exiting...'
+    print('exiting...')
+
 
 if __name__ == '__main__':
-    tgt = TicketGrantingTicket() # Ticket granting ticket
-    st = ServiceTicket() # Service Ticket
+    tgt = TicketGrantingTicket()  # Ticket granting ticket
+    st = ServiceTicket()  # Service Ticket
 
     try:
         if authentication():
-            shell_sim() # Start shell simulation
+            shell_sim()  # Start shell simulation
         else:
-            print 'Unsuccessful authentication.'
+            print('Unsuccessful authentication.')
     except (KeyboardInterrupt, SystemExit):
-        print "\nexisting..."
-        sys.exit(1) 
+        print("\nexisting...")
+        sys.exit(1)
     else:
         sys.exit(1)
--- federated_single_sign_on/sp/python/server/sp_ssh_server.py
+++ federated_single_sign_on/sp/python/server/sp_ssh_server.py
@@ -9,46 +9,47 @@ import sys
 import os
 import threading
 
+
 class ClientThread (threading.Thread):
     def __init__(self, channel, details):
         self.conn = channel
         self.addr = details
-        threading.Thread.__init__ ( self )
+        threading.Thread.__init__(self)
 
     def run(self):
-        print 'Received connection from ', self.addr[0]
-        f = open('ssh.key', 'r') # open ssh.key to read the key
+        print('Received connection from ', self.addr[0])
+        f = open('ssh.key', 'r')  # open ssh.key to read the key
         key_info = f.readline()
         key_info_list = key_info.split(';', 3)
-        
+
         st_info = self.conn.recv(1024)
         username, st = st_info.split(';', 1)
-        
+
         if key_info_list[3] == st:
             # Create home directory for the user if the directory doesn't exist
-            #d = os.path.dirname(username)
-            
+            # d = os.path.dirname(username)
+
             if os.path.exists(username):
                 os.chdir(username)
             else:
                 os.makedirs(username)
                 os.chdir(username)
-            
-            print "Authenticated the service ticket"
+
+            print("Authenticated the service ticket")
             self.conn.send('foreign remote shell~>')
-            print "Shell name sent"
-            
-            #while True:
+            print("Shell name sent")
+
+            # while True:
             try:
-                print 'Waiting for command'
+                print('Waiting for command')
 
                 command = self.conn.recv(1024)
-                print command
+                print(command)
                 result = ''
-                command_list = command.split(' ', 1)                    
-                
+                command_list = command.split(' ', 1)
+
                 if command_list[0] == 'ls':
-                    print 'Client request ls'
+                    print('Client request ls')
                     for dirname, dirnames, filenames in os.walk('.'):
                         for subdirname in dirnames:
                             result += os.path.join(dirname, subdirname)
@@ -57,100 +58,102 @@ class ClientThread (threading.Thread):
                             result += os.path.join(dirname, filename)
                             result += '\n'
                     self.conn.send(result)
-                    print 'results returned'
-                
+                    print('results returned')
+
                 elif command_list[0] == 'touch':
-                    #os.walk(username)
-                    print 'Client created file'
+                    # os.walk(username)
+                    print('Client created file')
                     f = open(command_list[1], 'w')
                     f.close
                     self.conn.send("File " + command_list[1] + " created")
-                    print 'results returned'
-                
+                    print('results returned')
+
                 elif command_list[0] == 'exit':
-                    print 'Client request exit'
+                    print('Client request exit')
                     self.conn.send('exit_confirm')
-                    print 'Exit confirmed'
-                
+                    print('Exit confirmed')
+
                 elif command_list[0] == 'hostname':
-                    print 'Client request hostname'
+                    print('Client request hostname')
                     self.conn.send(HOST)
-                    print 'Hostname returned'
-                
+                    print('Hostname returned')
+
                 elif command_list[0] == 'ifconfig':
-                    print 'Client request IP address'
+                    print('Client request IP address')
                     self.conn.send(IP)
-                    print 'IP address returned'
-                
+                    print('IP address returned')
+
                 else:
-                    print 'Client entered incorrect command'
+                    print('Client entered incorrect command')
                     self.conn.send('ls\tifconfig\thostname\texit\ttouch')
-                    print 'List of command returned'
-            
+                    print('List of command returned')
+
             except (KeyboardInterrupt, SystemExit):
                 return 0
-            
-            
+
             os.chdir("..")
         else:
-            print "Fail to authenticat the service ticket"
+            print("Fail to authenticat the service ticket")
             self.conn.send('fail')
-            print "Result of failed authentication sent"
-        
+            print("Result of failed authentication sent")
+
         self.conn.close()
 
-SERVICE_NAME = "ssh" # The service name
-HOST = socket.gethostname() # The host name
-DOMAIN = 'foreign.virtual.vm' # The domain name
-IP = socket.gethostbyname(HOST) # The IP address
 
-SSHD_HOST = socket.gethostname() # Get SSH Server Hostname
-SSHD_PORT = 2222 # Port to simuate ssh server
+SERVICE_NAME = "ssh"  # The service name
+HOST = socket.gethostname()  # The host name
+DOMAIN = 'foreign.virtual.vm'  # The domain name
+IP = socket.gethostbyname(HOST)  # The IP address
 
-KRB_HOST = socket.gethostname() # Get Kerberos key Distribution Centre Hostname
-KRB_PORT = 50001 # The KDC simulation's port number
+SSHD_HOST = socket.gethostname()  # Get SSH Server Hostname
+SSHD_PORT = 2222  # Port to simuate ssh server
+
+KRB_HOST = socket.gethostname()  # Get Kerberos key Distribution Centre Hostname
+KRB_PORT = 50001  # The KDC simulation's port number
+
 
 def sshd_sim():
     try:
-        s = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.bind(("127.0.0.1", SSHD_PORT))
         s.listen(1)
-        print 'SSH Server simulation started.'
-        
+        print('SSH Server simulation started.')
+
         # create a thread for every connection
         while True:
             channel, details = s.accept()
             c_th = ClientThread(channel, details)
             c_th.start()
-        
+
         s.close()
         sys.exit(0)
 
     except (KeyboardInterrupt, SystemExit):
         s.close()
-        print "\nKeyboard interrupted, existing..."
+        print("\nKeyboard interrupted, existing...")
         sys.exit(1)
 
     else:
         s.close()
-        sys.exit(1) 
+        sys.exit(1)
+
 
 def request_shared_key():
     # Get shared secure ticket from KDC (kdc_sim.py)
-    krb_socket = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+    krb_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     krb_socket.connect(("127.0.0.1", KRB_PORT))
     message = ''
     message += 'req_sk;'
-    message += (SERVICE_NAME +';')
+    message += (SERVICE_NAME + ';')
     message += (DOMAIN + ';')
     message += (IP)
 
-    krb_socket.send(message) # Send service information to KDC
-    share_key = krb_socket.recv(1024) # Recieve reply from KDC
+    krb_socket.send(message)  # Send service information to KDC
+    share_key = krb_socket.recv(1024)  # Recieve reply from KDC
 
     # Check the replied message
     if share_key != 'fail':
-        f = open(SERVICE_NAME + '.key', 'w') # Save the shared key
+        f = open(SERVICE_NAME + '.key', 'w')  # Save the shared key
         f.write(share_key)
         f.close
         krb_socket.close
@@ -159,11 +162,12 @@ def request_shared_key():
         krb_socket.close
         return False
 
+
 if __name__ == '__main__':
     if request_shared_key():
-        print 'Received shared key from KDC'
+        print('Received shared key from KDC')
         sshd_sim()
     else:
-        print 'Service was not found in KDC\'s service database.'
+        print('Service was not found in KDC\'s service database.')
 
     sys.exit(0)
